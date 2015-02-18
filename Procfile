c72af4d44c5b052662cbc66be0b2123b80bfd25a
Giuliano <giumendonca@gmail.com>
2015-02-18 15:27:21 -0200

    preparando para heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn concursinhos.wsgi --log-file -
--- concursinhos/settings.py
+++ concursinhos/settings.py
@@ -134,4 +134,9 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
+# Enable Connection Pooling
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
