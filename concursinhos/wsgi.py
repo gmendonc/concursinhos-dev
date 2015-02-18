9c5012ac7b56e61a12a21e31a38777fd2f8c2f36
Giuliano <giumendonca@gmail.com>
2015-02-18 15:28:11 -0200

    preparando para heroku

--- concursinhos/wsgi.py
+++ concursinhos/wsgi.py
@@ -15,3 +15,7 @@ from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
