a74b3097a17d26c770651c569ea279ca4aca9ed9
Giuliano <giumendonca@gmail.com>
2015-02-18 17:44:42 -0200

    teste3

--- concursinhos/urls.py
+++ concursinhos/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'concursinhos.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', include('quiz.urls')),
     url(r'^quiz/', include('quiz.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
